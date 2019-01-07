42ef9573a22c715522a3df67ee2b9357241fba5c
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-01-07 15:24:23 -0700

    Complete phase 2

--- main.js
+++ main.js
@@ -5,50 +5,77 @@ var imagesArr = JSON.parse(localStorage.getItem('photos')) || [];
 var reader = new FileReader();
 var title = document.querySelector('.input-title');
 var caption = document.querySelector('.input-caption');
+var deleteBtn = document.getElementById('delete-btn');
+var library = document.querySelector('.foto-library');
 
-window.addEventListener('load', appendPhotos);
+window.addEventListener('load', appendPhotos(imagesArr));
 addToAlbum.addEventListener('click', createElement);
+library.addEventListener('click', deleteCard);
 
-function appendPhotos() {
-  console.log("appendPhotos method");
-  imagesArr.forEach(function (photo) {
-    displayPhotoCard(photo.title, photo.file, photo.caption);
+function appendPhotos(array) {
+  imagesArr = [];
+  array.forEach(function (photo) {
+    var newPhoto = new Photo(photo.id, photo.title, photo.file, photo.caption, photo.favorite);
+    imagesArr.push(newPhoto);
+    displayPhotoCard(newPhoto);
   })
+  console.log(imagesArr);
 }
 
 function createElement() {
-  console.log(inputPhoto.files[0])
   if (inputPhoto.files[0]) {
     reader.readAsDataURL(inputPhoto.files[0]); 
-    reader.onload = addPhoto
+    reader.onload = addPhoto;
   }
 }
 
 function addPhoto(e) {
   // console.log(e.target.result);
-  var newPhoto = new Photo(Date.now(), e.target.result, title.value, caption.value);
+  var newPhoto = new Photo(Date.now(), title.value, e.target.result, caption.value);
   imagesArr.push(newPhoto);
   newPhoto.saveToStorage(imagesArr);
-  displayPhotoCard(newPhoto.title, newPhoto.file, newPhoto.caption);
+  displayPhotoCard(newPhoto.id, newPhoto.title, newPhoto.file, newPhoto.caption);
+  title.value = '';
+  caption.value = '';
 }
 
-function displayPhotoCard(title, file, caption) {
+function displayPhotoCard(card) {
   console.log("displayPhotoCard");
   photoGallery.innerHTML += 
-  `<article class="foto-card">
-      <section class="foto-title">
-        <h2>${title}</h2>
+  `<article class="foto-card" data-id="${card.id}">
+      <section>
+        <h2 class="input-fields foto-title" id="h2-edit" contenteditable="true">${card.title}</h2>
       </section>
       <section class="foto-image">
-        <img src=${file} />
+        <img src=${card.file} />
       </section>
-      <section class="foto-caption">
-        <p>${caption}</p>
+      <section class="input-fields foto-caption" contenteditable="true">
+        <p>${card.caption}</p>
       </section>
       <section class="foto-icons">
-        <button><img src="images/delete.svg"></button>
-        <button><img src="images/favorite.svg"></button>
+        <input class="card-btns" id="delete-btn" type="image" src="images/delete.svg">
+        <input class="card-btns" id="favorite-btn" type="image" src="images/favorite.svg">
       </section>
     </article>`
 }
 
+function deleteCard(event) {
+  console.log("clicked delete");
+  if (event.target.classList.contains('delete-btn')){
+    var indexToDelete = arrayCards.findIndex(function(idea) {
+      return idea.id === parseInt(event.target.id)
+    })
+    arrayCards[indexToDelete].deleteFromStorage(arrayCards, indexToDelete);
+    event.target.closest('.ideas__container').remove();
+  }
+}
+
+function manipulateCard(event) {
+  if (event.target.classList.contains('btn--kill')){
+    deleteCard(event);
+  } else if (event.target.classList.contains('btn--dwn')) {
+    changeQuality(event);
+  } else if (event.target.classList.contains('btn--up')) {
+    changeQuality(event);
+  }
+}
--- photo.js
+++ photo.js
@@ -1,13 +1,21 @@
 class Photo {
-  constructor(id, file, title, caption, favorite) {
+  constructor(id, title, file, caption, favorite) {
     this.id = id;
-    this.file = file;
     this.title = title;
+    this.file = file;
     this.caption = caption;
     this.favorite = favorite || false;
   }
 
   saveToStorage(imagesArr) {
     localStorage.setItem('photos', JSON.stringify(imagesArr));
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updatePhoto() {
+
   }
 }
